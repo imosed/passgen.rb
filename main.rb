f4a87e9298902b87b6dbc47dcd9b3e51c29dcb6f
Jared <imosed@users.noreply.github.com>
2022-04-06 00:52:32 -0400

    Reorganize code

--- main.rb
+++ main.rb
@@ -23,8 +23,7 @@ def generate(length, pool, max_symbols, unique_chars = false)
   pool.reject! { |e| e.char == password } if unique_chars
   symbol_count = 0
 
-  i = 0
-  while i < length
+  (0...length).each do |_|
     pool = pool.shuffle
     chosen = pool[rand(pool.length)]
     password += chosen.char
@@ -33,7 +32,6 @@ def generate(length, pool, max_symbols, unique_chars = false)
       pool.reject! { |e| e.type == Type::SYMBOL } if symbol_count == max_symbols
     end
     pool.reject! { |e| e.char == chosen.char } if unique_chars
-    i += 1
   end
 
   password
@@ -57,60 +55,7 @@ def parse_args
   all_args
 end
 
-pool = [
-  Character.new('a', Type::LOWER), Character.new('b', Type::LOWER),
-  Character.new('c', Type::LOWER), Character.new('d', Type::LOWER),
-  Character.new('e', Type::LOWER), Character.new('f', Type::LOWER),
-  Character.new('g', Type::LOWER), Character.new('h', Type::LOWER),
-  Character.new('i', Type::LOWER), Character.new('j', Type::LOWER),
-  Character.new('k', Type::LOWER), Character.new('l', Type::LOWER),
-  Character.new('m', Type::LOWER), Character.new('n', Type::LOWER),
-  Character.new('o', Type::LOWER), Character.new('p', Type::LOWER),
-  Character.new('q', Type::LOWER), Character.new('r', Type::LOWER),
-  Character.new('s', Type::LOWER), Character.new('t', Type::LOWER),
-  Character.new('u', Type::LOWER), Character.new('v', Type::LOWER),
-  Character.new('w', Type::LOWER), Character.new('x', Type::LOWER),
-  Character.new('y', Type::LOWER), Character.new('z', Type::LOWER),
-  Character.new('A', Type::UPPER), Character.new('B', Type::UPPER),
-  Character.new('C', Type::UPPER), Character.new('D', Type::UPPER),
-  Character.new('E', Type::UPPER), Character.new('F', Type::UPPER),
-  Character.new('G', Type::UPPER), Character.new('H', Type::UPPER),
-  Character.new('I', Type::UPPER), Character.new('J', Type::UPPER),
-  Character.new('K', Type::UPPER), Character.new('L', Type::UPPER),
-  Character.new('M', Type::UPPER), Character.new('N', Type::UPPER),
-  Character.new('O', Type::UPPER), Character.new('P', Type::UPPER),
-  Character.new('Q', Type::UPPER), Character.new('R', Type::UPPER),
-  Character.new('S', Type::UPPER), Character.new('T', Type::UPPER),
-  Character.new('U', Type::UPPER), Character.new('V', Type::UPPER),
-  Character.new('W', Type::UPPER), Character.new('X', Type::UPPER),
-  Character.new('Y', Type::UPPER), Character.new('Z', Type::UPPER),
-  Character.new('1', Type::NUMBER), Character.new('2', Type::NUMBER),
-  Character.new('3', Type::NUMBER), Character.new('4', Type::NUMBER),
-  Character.new('5', Type::NUMBER), Character.new('6', Type::NUMBER),
-  Character.new('7', Type::NUMBER), Character.new('8', Type::NUMBER),
-  Character.new('9', Type::NUMBER), Character.new('0', Type::NUMBER),
-  Character.new('/', Type::SYMBOL), Character.new('!', Type::SYMBOL),
-  Character.new('@', Type::SYMBOL), Character.new('#', Type::SYMBOL),
-  Character.new('$', Type::SYMBOL), Character.new('%', Type::SYMBOL),
-  Character.new('^', Type::SYMBOL), Character.new('&', Type::SYMBOL),
-  Character.new('*', Type::SYMBOL), Character.new('(', Type::SYMBOL),
-  Character.new(')', Type::SYMBOL), Character.new('[', Type::SYMBOL),
-  Character.new(']', Type::SYMBOL), Character.new('{', Type::SYMBOL),
-  Character.new('}', Type::SYMBOL), Character.new(':', Type::SYMBOL),
-  Character.new(';', Type::SYMBOL), Character.new('<', Type::SYMBOL),
-  Character.new('>', Type::SYMBOL), Character.new(',', Type::SYMBOL),
-  Character.new('.', Type::SYMBOL), Character.new('/', Type::SYMBOL),
-  Character.new('?', Type::SYMBOL), Character.new('`', Type::SYMBOL),
-  Character.new('~', Type::SYMBOL), Character.new(' ', Type::SYMBOL)
-]
-
 params = parse_args
-
-length = if params.key?('length')
-           [Integer(params['length']), 40].min
-         else
-           16
-         end
 
 if params.key?('help')
   puts 'passgen.rb'
@@ -123,8 +68,34 @@ if params.key?('help')
   puts '  --no-upper              Disallow uppercase letters in password generation.'
   puts '  --unique-chars          Characters can only be used once in password generation.'
   puts '  --max-symbols=NUMBER    Limit the amount of symbols to NUMBER.'
+  puts '  --shuffles=NUMBER       Shuffles the password NUMBER of times.'
   exit
 end
+
+pool = (32..126).collect do |i|
+  if i.between?(65, 90) # ASCII uppercase
+    char_type = Type::UPPER
+  elsif i.between?(97, 122) # ASCII lowercase
+    char_type = Type::LOWER
+  elsif i.between?(48, 57) # ASCII digits
+    char_type = Type::NUMBER
+  else # ASCII symbols
+    char_type = Type::SYMBOL
+  end
+  Character.new(i.chr, char_type)
+end
+
+pool.reject! { |c| c.type == Type::SYMBOL } if params.key?('no-symbols')
+pool.reject! { |c| c.type == Type::NUMBER } if params.key?('no-digits') ||
+  params.key?('no-numbers')
+pool.reject! { |c| c.type == Type::UPPER } if params.key?('no-upper')
+pool.reject! { |c| c.type == Type::LOWER } if params.key?('no-lower')
+
+length = if params.key?('length')
+           [Integer(params['length']), 60].min
+         else
+           16
+         end
 
 if params.key?('no-symbols') && params.key?('no-digits') &&
    params.key?('no-upper') && params.key?('no-lower')
@@ -132,11 +103,10 @@ if params.key?('no-symbols') && params.key?('no-digits') &&
   exit
 end
 
-pool.reject! { |c| c.type == Type::SYMBOL } if params.key?('no-symbols')
-pool.reject! { |c| c.type == Type::NUMBER } if params.key?('no-digits') ||
-                                               params.key?('no-numbers')
-pool.reject! { |c| c.type == Type::UPPER } if params.key?('no-upper')
-pool.reject! { |c| c.type == Type::LOWER } if params.key?('no-lower')
-max_symbols = Integer(params['max-symbols']) if params.key?('max-symbols')
-pw = generate(length, pool, max_symbols || length, params.key?('unique-chars')).chars.shuffle
+max_symbols = (Integer(params['max-symbols']) if params.key?('max-symbols')) || length
+shuffles = (Integer(params['shuffles']) if params.key?('shuffles')) || 0
+pw = generate(length, pool, max_symbols, params.key?('unique-chars')).chars
+(0..shuffles).each do |_|
+  pw.shuffle!
+end if params.key?('shuffles') and shuffles > 0
 puts pw.join
